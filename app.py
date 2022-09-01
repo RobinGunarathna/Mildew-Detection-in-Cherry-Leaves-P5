5ee67e6ac4b4ad78c14a1dc4f438a1d5f01deaf7
RobinGunarathna <madgunrob@gmail.com>
2022-09-01 20:54:13 +0000

    setting up streamlit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from multi_page_app.multi_page import MultiPage
+from multi_page_app.page_summary import page_summary_body
+from multi_page_app.Mildew_and_Healthy_Visualizer import page_cells_visualizer_body
+from multi_page_app.Mildew_detector import page_malaria_detector_body
+from multi_page_app.project_hypothesis import page_project_hypothesis_body
+from multi_page_app.ml_performance import page_ml_performance_metrics
+
+app = MultiPage(app_name= "Mildew Detector")
+
+app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Cherry leave Visualizer", page_cells_visualizer_body)
+app.add_page("Mildew Detection", page_malaria_detector_body)
+app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("ML Performance Metrics", page_ml_performance_metrics)
+
+app.run() # Run the  app
